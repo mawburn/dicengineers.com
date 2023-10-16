05f9c8a0dd47a15973569259017ff7e9e63b84d0
mawburn <mawburn7@gmail.com>
2023-10-16 19:41:23 -0400

    add showcase ogimg

--- src/app/showcase/page.tsx
+++ src/app/showcase/page.tsx
@@ -1,5 +1,18 @@
+import { Metadata } from 'next'
 import { Layout } from 'src/components/Layout'
 import { ShowCaseList } from 'src/components/ShowCaseList'
+
+export const metadata: Metadata = {
+  title: 'Showcase - Dicengineers',
+  description: 'Showcase of projects by members of Dicengineers',
+  openGraph: {
+    images: [
+      { url: '/showcaseog.webp', width: 1600, height: 800, alt: 'Dicengineers.com Showcase' },
+      { url: '/showcaseog.jpg', width: 1600, height: 800, alt: 'Dicengineers.com Showcase' },
+    ],
+    url: 'https://dicengineers.com',
+  },
+}
 
 export default function ShowCase() {
   return (
