20a1586692b1ad2553e301e09c74aedf3aa9708d
matt burnett <mawburn7@gmail.com>
2023-10-20 16:41:08 -0400

    update summaries

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,101 +1,154 @@
 import fetch from 'node-fetch'
+import ExtApi from 'openai'
+import messagesJson from './data.json'
 
 const MAX_TOKENS = 81912
 
-export async function GET() {
-  const result: any[] = []
+export async function GET(req: Request) {
+  const apikey = req.headers.get('Authorization')
 
-  let lastId = 1163487291315073134
-
-  let end = false
-
-  let count = 0
-
-  while (!end) {
-    const res = await fetch(
-      `https://discord.com/api/channels/1163251543861100615/messages?after=${lastId}`,
-      {
-        headers: {
-          Authorization: `Bot ${process.env.DISCORD}`,
-        },
-      }
-    )
-
-    if (res.status === 429) {
-      const retryAfter = res.headers.get('Retry-After')
-      if (retryAfter) {
-        await new Promise(resolve => setTimeout(resolve, Number(retryAfter)))
-        continue
-      }
-    }
-
-    if (!res.ok) {
-      end = true
-      continue
-    }
-
-    const channel = (await res.json()) as any[]
-
-    if (channel.length === 0) {
-      end = true
-      continue
-    }
-
-    result.push(...channel)
-    lastId = channel[channel.length - 1].id
+  if (apikey !== process.env.KEY) {
+    return Response.json({ error: 'Not found' }, { status: 404 })
   }
 
-  let dupeId = 0
+  // const result: any[] = []
 
-  const grouped = result
-    .sort((a: any, b: any) => a.id - b.id)
-    .filter(obj => {
-      if (obj.id !== dupeId) {
-        dupeId = obj.id
-        return true
-      }
-      return false
-    })
-    .reduce((acc, obj) => {
-      const date = new Date(obj.timestamp)
-      const key = date.toLocaleDateString('en-US', {
-        month: 'long',
-        day: 'numeric',
-        year: 'numeric',
-      })
+  // let lastId = 1163487291315073134
 
-      if (!acc[key]) {
-        acc[key] = []
-      }
+  // let end = false
 
-      acc[key].push(`${obj.author.username}: ${obj.content}`)
+  // while (!end) {
+  //   const res = await fetch(
+  //     `https://discord.com/api/channels/1163251543861100615/messages?after=${lastId}`,
+  //     {
+  //       headers: {
+  //         Authorization: `Bot ${process.env.DISCORD}`,
+  //       },
+  //     }
+  //   )
 
-      return acc
-    }, {})
+  //   if (res.status === 429) {
+  //     const retryAfter = res.headers.get('Retry-After')
+  //     if (retryAfter) {
+  //       await new Promise(resolve => setTimeout(resolve, Number(retryAfter)))
+  //       continue
+  //     }
+  //   }
 
-  const messages: Record<string, string[]> = {}
+  //   if (!res.ok) {
+  //     end = true
+  //     continue
+  //   }
 
-  Object.keys(grouped).forEach(key => {
-    const _msg = grouped[key]
-      .join('\n')
-      .replace(/<[^>]*>|(\|\|)[^|]*(\|\|)/g, '')
-      .replace(/\[([^\]]+)\]\([^\)]+\)/g, '$1')
+  //   const channel = (await res.json()) as any[]
 
-    let tokens = _msg.split(' ')
-    const messageArray = []
+  //   if (channel.length === 0) {
+  //     end = true
+  //     continue
+  //   }
 
-    while (tokens.length > MAX_TOKENS) {
-      const slice = tokens.slice(0, MAX_TOKENS)
-      messageArray.push(slice.join(' '))
-      tokens = tokens.slice(MAX_TOKENS)
-    }
+  //   result.push(...channel)
+  //   lastId = channel[channel.length - 1].id
+  // }
 
-    if (tokens.length > 0) {
-      messageArray.push(tokens.join(' '))
-    }
+  // let dupeId = 0
 
-    messages[key] = messageArray as string[]
+  // const grouped = result
+  //   .sort((a: any, b: any) => a.id - b.id)
+  //   .filter(obj => {
+  //     if (obj.id !== dupeId) {
+  //       dupeId = obj.id
+  //       return true
+  //     }
+  //     return false
+  //   })
+  //   .reduce((acc, obj) => {
+  //     const date = new Date(obj.timestamp)
+  //     const key = date.toLocaleDateString('en-US', {
+  //       month: 'long',
+  //       day: 'numeric',
+  //       year: 'numeric',
+  //     })
+
+  //     if (!acc[key]) {
+  //       acc[key] = []
+  //     }
+
+  //     acc[key].push(`${obj.author.username}: ${obj.content}`)
+
+  //     return acc
+  //   }, {})
+
+  // const messages: Record<string, string[]> = {}
+
+  // Object.keys(grouped).forEach(key => {
+  //   const _msg = grouped[key]
+  //     .join('\n')
+  //     .replace(/<[^>]*>|(\|\|)[^|]*(\|\|)/g, '')
+  //     .replace(/\[([^\]]+)\]\([^\)]+\)/g, '$1')
+
+  //   let tokens = _msg.split(' ')
+  //   const messageArray = []
+
+  //   while (tokens.length > MAX_TOKENS) {
+  //     const slice = tokens.slice(0, MAX_TOKENS)
+  //     messageArray.push(slice.join(' '))
+  //     tokens = tokens.slice(MAX_TOKENS)
+  //   }
+
+  //   if (tokens.length > 0) {
+  //     messageArray.push(tokens.join(' '))
+  //   }
+
+  //   messages[key] = messageArray as string[]
+  // })
+
+  const extApi = new ExtApi({
+    apiKey: process.env.API_KEY,
+    maxRetries: 10,
   })
 
-  return Response.json(messages)
+  const summary: { date: string; summary: string }[] = []
+
+  const messages = messagesJson as Record<string, string[]>
+
+  let start = true
+
+  for (const k in messages) {
+    if (!start) {
+      await new Promise(resolve => setTimeout(resolve, 10000))
+    }
+    start = false
+
+    const messageArr = messages[k]
+
+    for (const content of messageArr) {
+      const aiMessages: any[] = [
+        {
+          role: 'system',
+          content:
+            'Give me the BRIEFEST possible summary of the conversation in the given chat and return it in a bulleted list. You do not need to do line by line, you can combine multiple messages from a user and just give an overall VERY BRIEF summary of the conversation. The format will be:\n(username):(message)\n(username:)(message)\n and so on. Do not include any personal information, except usernames and urls. Wrap usernames in double stars ie: **username** when used.',
+        },
+        {
+          role: 'user',
+          content,
+        },
+      ]
+
+      const aiRes = await extApi.chat.completions.create({
+        model: 'gpt-4',
+        messages: aiMessages,
+        temperature: 0.5,
+        max_tokens: 500,
+        frequency_penalty: 1,
+        presence_penalty: 0.25,
+      })
+
+      if (aiRes.choices[0].message.content) {
+        summary.push({ date: k, summary: aiRes.choices[0].message.content })
+      }
+    }
+  }
+
+  return Response.json(summary)
 }
